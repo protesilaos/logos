37138ce54044d04eaad5d5f81d42eefceef0df01
Protesilaos Stavrou <info@protesilaos.com>
2022-03-11 17:08:55 +0200

    Bump version

--- logos.el
+++ logos.el
@@ -4,7 +4,7 @@
 
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://gitlab.com/protesilaos/logos
-;; Version: 0.1.0
+;; Version: 0.1.2
 ;; Package-Requires: ((emacs "27.1"))
 
 ;; This file is NOT part of GNU Emacs.
