36e911064c263813110b17acd5fb9b01b9436112
Protesilaos Stavrou <info@protesilaos.com>
2022-03-17 15:10:53 +0200

    Upgrade to version 0.2.0

--- logos.el
+++ logos.el
@@ -4,7 +4,7 @@
 
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://gitlab.com/protesilaos/logos
-;; Version: 0.1.2
+;; Version: 0.2.0
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: convenience, focus, writing, presentation, narrowing
 
