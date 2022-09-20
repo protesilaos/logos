be3333b6969c34bb3317d11f1daef6aa5af476cb
Protesilaos Stavrou <info@protesilaos.com>
2022-09-20 05:40:01 +0300

    Guard against void outline-regexp
    The previous value would cause a problem if the outline-regexp had not
    been set yet.  In a typical Emacs session, even on 'emacs -Q', the
    outline-regexp is set in the scratch buffer, so we always have a valid
    value.  However, when the user changes the initial-buffer-choice or
    opens Emacs directly for a given file, the outline-regexp can be void,
    causing an error with logos-outline-regexp-alist.
    
    Thanks to Xiaoduan for informing me about this error and for testing my
    code on how to fix it.  This was done via a private channel and is
    shared with permission.

--- logos.el
+++ logos.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Logos Development <~protesilaos/logos@lists.sr.ht>
 ;; URL: https://git.sr.ht/~protesilaos/logos
 ;; Mailing-List: https://lists.sr.ht/~protesilaos/logos
-;; Version: 0.5.0
+;; Version: 0.5.1
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: convenience, focus, writing, presentation, narrowing
 
@@ -114,7 +114,7 @@ When this variable is nil, pages are demarcated by the
   `((emacs-lisp-mode . "^;;;+ ")
     (org-mode . "^\\*+ +")
     (markdown-mode . "^\\#+ +")
-    (t . ,(or outline-regexp logos--page-delimiter)))
+    (t . ,(if (boundp 'outline-regexp) outline-regexp logos--page-delimiter)))
   "Alist of major mode and regular expression of the outline.
 Only used when `logos-outlines-are-pages' is non-nil.
 
